7ca8f183cf76d3b017e15127e0c6014313326e81
UphengV <uvue@appdev.yearup.org>
2025-04-11 14:50:04 -0400

    cal 2

---
+++ Week1FinancialCalculator/src/main/java/org/example/Calculator2.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculator2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your initial deposit?");
+        double p = scanner.nextDouble();
+        System.out.println("What is your annual interest rate?");
+        double r = scanner.nextDouble();
+        System.out.println("Number of years");
+        double t = scanner.nextDouble();
+        //FV = P(1 + r/n)^(n*t)
+        double n = 365;
+        double d = r / 100;
+        //double r2 = r / n;
+
+        double fV = p * Math.pow(1 + (d / n), n * t);
+
+        //Total Interest = fV - P
+
+        double totalInterest = fV - p;
+
+        System.out.printf("Your ending value is %.2f and your earned interest is %.2f" , fV , totalInterest);
+
+
+    }
+}
--- Week1FinancialCalculator/src/main/java/org/example/Main.java
+++ Week1FinancialCalculator/src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your Principal?");
+        double amountP = scanner.nextDouble();
+        System.out.println("What is your interest rate?");
+        double interestR = scanner.nextDouble();
+        System.out.println("what is your loan length?");
+        double loanL = scanner.nextDouble();
+        //Monthly Payment,Principal,Rate,Length
+
+        //monthlyP = amountP *(interestR^(1+interestR)^numberMp)-1);
+        //total interest = (monthlyP *numberMp)-P
+        // interestR / 100 TURNS IT INTO A DECIMAl
+        double monthlyRate = (interestR / 100 / 12);
+        double months = loanL * 12;
+
+        double monthlyP = amountP * (monthlyRate *Math.pow(1+ monthlyRate, months)) / (Math.pow(1+monthlyRate, months)-1);
+        //used Math.pow twice because if you do the same for the first you do again
+
+
+        //total interest = (M * n) - P
+
+        double anwser = (monthlyP * months) - amountP;
+        System.out.printf("Your monthly payment is $%.2f and your interest you will pay $%.2f", monthlyP, anwser);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
